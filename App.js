d8f8d6b513091ae197e14f947c30408d31bac43d
Timur <zairovne@gmail.com>
2022-09-17 14:34:43 +0500

    init project add expo fonts and SplashScreen

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
+import {useEffect, useCallback} from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import * as SplashScreen from 'expo-splash-screen'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'rubik-light': require('./assets/fonts/Rubik-Light.ttf'),
+    'rubik-medium': require('./assets/fonts/Rubik-Medium.ttf'),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{ fontFamily: 'Inter-Black', fontSize: 30 }}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
