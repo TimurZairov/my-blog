4e968059c5206423772514cbf30f23750a5d22f5
Timur <zairovne@gmail.com>
2022-09-18 15:50:06 +0500

    Gradient, Navigation menu JSX Верстка навигации главной страницы и бэк градиинет с добавлением экнонк

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,22 +1,42 @@
 import React from "react";
-import {Button, Platform, StatusBar, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {Button, Image, Platform, StatusBar, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {THEME} from "../theme";
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { LinearGradient } from 'expo-linear-gradient';
+import {LinearGradient} from 'expo-linear-gradient';
 
-export const MainScreen =({navigation}) => {
 
-    const goToPosts = () => {
-        navigation.navigate('Посты')
-    }
+export const MainScreen = ({navigation}) => {
+
+    // const goToPosts = () => {
+    //     navigation.navigate('Посты')
+    // }
 
     return (
         <View style={styles.AndroidSaveArea}>
-            <Ionicons  name={ 'menu'}/>
-            <View >
-                <TouchableOpacity onPress={goToPosts} activeOpacity={0.8} >
+            <LinearGradient
+                // Background Linear Gradient
+                colors={[THEME.MAIN_BG, THEME.WHITE_BG]}
+                style={styles.background}
+                start={{
+                    x: 0.2499,
+                    y: 0
+                }}
+                end={{
+                    x: 0.25,
+                    y: 0.0
+                }}
+            />
+            <View style={styles.nav}>
+
+                <TouchableOpacity activeOpacity={0.5}>
+                    <Ionicons name={'menu'} size={32} color={THEME.MAIN_FONT_COLOR}/>
                 </TouchableOpacity>
-                <Button title="Go to Posts"  onPress={() => navigation.push('Посты')} />
+                <View style={styles.wrap}>
+                    <Ionicons name="search" size={30} color={THEME.MAIN_FONT_COLOR}/>
+                    <Image source={require('../../assets/images/avatar.jpg')} style={styles.imgAvatar}/>
+                </View>
+
+                {/*<Button title="Go to Posts"  onPress={() => navigation.push('Посты')} />*/}
             </View>
         </View>
 
@@ -27,17 +47,31 @@ const styles = StyleSheet.create({
     AndroidSaveArea: {
         flex: 1,
         paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-        backgroundColor: 'red'
+        position: "relative"
     },
-    box: {
-        width: 50,
-        height: 50,
-        backgroundColor: THEME.POST_BG_COLOR,
-        borderRadius: 50,
+    background: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0
     },
-    burger: {
-        width: 30,
-        height: 30,
-        backgroundColor: THEME.MAIN_BG
-    }
+    nav: {
+        height: 60,
+        paddingHorizontal: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    wrap: {
+        width: '35%',
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    imgAvatar: {
+        width: 60,
+        height: 60,
+        borderRadius: 100
+    },
 });
